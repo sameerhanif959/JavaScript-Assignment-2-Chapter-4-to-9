7f7b8221ef3a8a8d3e7f8176086d7ce450930a50
Sameer Hanif <141715583+sameerhanif959@users.noreply.github.com>
2024-03-07 10:11:37 +0500

    Add files via upload

---
+++ java-script-2/app.js
@@ -0,0 +1,322 @@
+                                   
+                        //---------------   CHAPTER NO 4  ------------- //
+
+                        // Question  1 //
+
+                        // START //
+                       
+                    //     var student_name="Sameer Safwan "; insititute = "Saylani MASS IT Training Program ";   course = "Meta Web And Development ";
+                      
+                    //     alert(student_name + insititute +  course )
+                       
+
+
+
+                        // END //
+
+                             // Question  2 //
+
+                        // Legal variable names: //
+
+                                    // START //
+
+                         // var  userName
+                         // var  userAge
+                         // var  firstName
+                         // var  lastName
+                         // var  totalAmount
+                         
+
+                        //  Illegal variable names: //
+
+                    //     var alert;
+                    //     var  user-name 
+                    //     var  Number
+                    //     var   function 
+                    //     var   break
+                       
+                                      // END //
+
+    
+                             // Question  3 //
+
+                               // START //
+
+     //    document.write("<h1> “Rules for naming JS variables”</h1>");
+
+     //    document.write(" <p> Variable names can only contain number S and _ For example $my_1stVariable </p>" );
+
+     //    document.write(" <p> Variables must begin with a latter S or .For example $name, _name or name </p>")
+
+     //    document.write(" <p> Variable names are case sensitive. </p>")
+
+     //    document.write(" <p> Variable names should not be JS Keywords </p>")
+
+                               // END //
+                
+               
+                               //---------------   CHAPTER NO 5  ------------- //
+
+                         
+
+                              // Question 1 //
+ 
+
+                              // START //
+
+                                  
+                                  // var a = 3;
+                                  // var b = 5;
+                                  // var c = a + b;
+                                  // document.write("<b>Sum of </b>" + a + "<b> and </b>" + b + "<b> is </b>" + c);
+                             
+
+                               // END //
+                              
+        
+
+
+                              // Question  2 //
+
+                                
+                               // START //
+
+                              
+     //    var num1 = prompt("Enter the first number:");
+     //    var num2 = prompt("Enter the second number:");
+
+     //    num1 = parseFloat(num1);
+     //    num2 = parseFloat(num2);
+
+        
+     //    var subtraction = num1 - num2;
+     //    var multiplication = num1 * num2;
+     //    var division = num1 / num2;
+     //    var modulus = num1 % num2;
+
+     //    document.write("<h2>Arithmetic Operations:</h2>");
+     //    document.write("<p>" + num1 + " - " + num2 + " = " + subtraction + "</p>");
+     //    document.write("<p>" + num1 + " * " + num2 + " = " + multiplication + "</p>");
+     //    document.write("<p>" + num1 + " / " + num2 + " = " + division + "</p>");
+     //    document.write("<p>" + num1 + " % " + num2 + " = " + modulus + "</p>");
+
+                                 // END //
+
+                         // Question  3 //
+
+                          // START //
+
+                          // var ticketPrice = 600;
+                          // var numofticket = 5;
+                          
+                          // var totaltecket =   ticketPrice * numofticket;
+
+                          // document.write("<h2>Movie Ticket Cost Calculation</h2>")
+                          // document.write("<p>Cost of one movie ticket is " + ticketPrice + " PKR. 3000</p>");
+                          // document.write("<p>Cost of buying " + numofticket + " tickets to a movie is " + otaltecket + " PKR.3000</p>");
+
+                                      
+                                                    // END //
+
+                                    //    Question 4  //
+                                    
+                                    // Start  //
+
+                                            // // Store the ticket price in a variable
+        // var ticketPrice = 600;
+
+        // // Calculate the cost of buying 5 tickets
+        // var numOfTickets = 5;
+        // var totalCost = ticketPrice * numOfTickets;
+
+     
+        // document.write("<h2>Movie Ticket Cost Calculation</h2>");
+        // document.write("<p>Cost of one movie ticket is " + ticketPrice + " PKR.</p>");
+        // document.write("<p>Cost of buying " + numOfTickets + " tickets to a movie is " + totalCost + " PKR.</p>");
+ 
+                                          // END  //
+
+
+
+                                            //    Question 5  //
+
+                                                 // Start  //
+                                      
+                                //   document.write("<h1> Table of 6 </h1>")  
+                                  
+                                //   var table = 6 * 1;
+
+                                //   document.write("<p> 6 x 1 = " + table + "</p>" );
+                                //   table = 6 * 2
+                                //   document.write(" <p> 6 x 2 = " + table + "</p>" );
+                                //   table = 6 * 3
+                                //   document.write(" <p>6 x 3 = " + table  + "</p>" );
+                                //   table = 6 * 4
+                                //   document.write(" <p>6 x 4 = " + table + "</p>" );
+                                //   table = 6 * 5
+                                //   document.write(" <p>6 x 5 = " + table + "</p>" );
+                                //   table = 6 * 6
+                                //   document.write("6 x 6 = " + table  + "</p>");
+                                //   table = 6 * 7
+                                //   document.write("6 x 7 = " + table  + "</p>");
+                                //   table = 6 * 8
+                                //   document.write("6 x 8 = " + table + "</p>" );
+                                //   table = 6 * 9
+                                //   document.write("6 x 9 = " + table  + "</p>");
+                                //   table = 6 * 10
+                                //   document.write("6 x 10 = " + table  + "</p>");
+                                  
+                                                 
+                                          // END  //
+
+                                             //    Question 6  //
+
+                                              // Start  //
+
+                                            //   var Celsius = 26 + "°C"
+
+                                            //   var check = (26 * 9/5) + 32
+
+                                              
+                                            //   document.write( "<p>" + Celsius + "is " + check + "°F" + "</p>")
+
+                                            //   var Fahrenheit = 78.8 +  "°F"
+
+                                            //   var check2 = (78.8 - 32 ) * 5/9
+
+                                            //   document.write(Fahrenheit + "is " +  check2 + "°C" )
+
+
+
+                                               // END  //
+
+                                                  //    Question 7 //
+
+                                                  
+                                                    // START  //
+
+
+                              //          //a. Price of item 1.
+                              //    var item1 = 650;
+                              //    // b. Price of item 2
+                              //   var item2 = 100;
+                              //    // c. Ordered quantity of item 1
+                              //   var qty_item1 = 3;
+                              //  // d. Ordered Quantity of item 2
+                              //  var qty_item2 = 7;
+                              //  // e. Shipping charges
+                              //  var ship_charges=100;
+
+
+                              //  var total_cost = (item1 * qty_item1) + (item2 * qty_item2) + ship_charges; 
+                              //  document.write("<h1> Shopping Cart </h1>");
+                              //  document.write("Price of item 1 is " + item1 + "</br>");
+                              //  document.write("Quaintity of item 1 is " + qty_item1 + "</br>");
+                              //  document.write("Price of item 2 is " + item2 + "</br>");
+                              //  document.write("Quaintity of item 2 is " + qty_item2 + "</br></br></br></br>");
+                              //  document.write("Total cost of your order is " + total_cost);
+
+                            // END  //
+    
+     
+
+                                  // Question 8 //
+                                      
+                                   // START //
+
+                    //  document.write("<h1> Marks Sheet </h1>")              
+
+                    // var mark = 100;
+
+                    // var totalMark = 200;
+
+                    // var par = (100 * 100 / 200 ) 
+
+                    // document.write("<p> total marks is " + totalMark + "</p>")
+
+                    // document.write(" <p> obtain marks is " + mark + "</p>")
+
+                    // document.write(" <p> percentage is " + par + " %</p>" )
+
+                                  // END //
+
+
+
+                            // Question  9 //
+                        
+                              // START //
+
+
+// var usaAmount = 10;
+//     var ukAmount =  30;
+
+//     var usaToPkrRate = 278.33;
+//     var ukToPkrRate = 352.28;
+
+//     var totalPkr = (usaAmount * usaToPkrRate) +  (ukAmount  *  ukToPkrRate ) ;
+
+//     document.write("<h2>Currency Conversion</h2>");
+
+//     document.write(" Total  Currency in PKR " + totalPkr  );
+
+
+                            // END //
+
+                             // Question  10 //
+
+                               // START //
+                                
+                               // var num = 5
+                                // var num = num * 10
+                                // var num = num / 2
+                                // document.write("<h1>"+num+"<h1>")                                
+                          
+
+                                        // END //
+                          
+                                                                             
+                                                    // Question  11 //
+                                                       
+                                                       // START  //
+
+                                    //   var currentYear = 2024
+                                    //  var birthYear = 2002
+                                    //  var myAge = currentYear - birthYear
+                                    //  document.write("<h1>"+myAge+"</h1>")
+
+                                    // END //
+
+                                              
+                                    // Question  12 //
+                                     
+                                        // START  //
+
+                                //  var pi = 3.142;
+                                //  var radius = 20;
+                                //  var circumference = 2 * pi * radius;
+                                //  console.log(circumference)
+                                //  var area = pi * radius ** 2;
+                                //  console.log(area)
+                                //  document.write("<h1>"+"The circumference is: "+circumference+"</h1>");
+                                //  document.write("<h1>"+"The area is: "+area+"</h1>");
+
+
+                                       // END //
+
+
+
+                                  // Question  13 //
+
+                                   // START  //
+
+
+                            //  var favSnacks = "Chocoday"
+                            //  var myAge = 28
+                            //  var featureAge = 60
+                            //  var snacksAmount = 2
+                            //  var totalSnacksNowTillDeath = (featureAge - myAge) * snacksAmount
+
+                            //  document.write("<h1>"+"I'll need "+totalSnacksNowTillDeath+" "+ favSnacks+" to last you until the ripe old age of 60"+"</h1>")
+
+
+                                       // END //
